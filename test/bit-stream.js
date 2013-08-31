db087ce4ab1efe9d97d85f88beecbb16def5aad6
Jon Spencer <jon@jonspencer.ca>
2013-08-31 16:22:04 -0700

    a couple of universal codes

--- test/bit-stream.js
+++ test/bit-stream.js
@@ -22,4 +22,46 @@ describe('BitStream', function() {
 			assert(bs.readBits(written[i].bits) == written[i].value);
 		}
 	})
+	it("unary coding works", function() {
+		bs.index = 0;
+		var written = [];
+		for (var i = 0; i < 50; ++i) {
+			var bits = Math.random() * 10 | 0;
+			var value = Math.random() * (1 << bits) + 1 | 0;
+			bs.writeUnary(value);
+			written.push(value);
+		}
+		bs.index = 0;
+		for (var i = 0; i < written.length; ++i) {
+			assert.equal(bs.readUnary(), written[i]);
+		}
+	})
+	it("elias-gamma coding works", function() {
+		bs.index = 0;
+		var written = [];
+		for (var i = 0; i < 200; ++i) {
+			var bits = Math.random() * 30 | 0;
+			var value = Math.random() * (1 << bits) + 1 | 0;
+			bs.writeEliasGamma(value);
+			written.push(value);
+		}
+		bs.index = 0;
+		for (var i = 0; i < written.length; ++i) {
+			assert.equal(bs.readEliasGamma(), written[i]);
+		}
+	})
+	it("elias-delta coding works", function() {
+		bs.index = 0;
+		var written = [];
+		for (var i = 0; i < 200; ++i) {
+			var bits = Math.random() * 30 | 0;
+			var value = Math.random() * (1 << bits) + 1 | 0;
+			bs.writeEliasDelta(value);
+			written.push(value);
+		}
+		bs.index = 0;
+		for (var i = 0; i < written.length; ++i) {
+			assert.equal(bs.readEliasDelta(), written[i]);
+		}
+	})
 });
